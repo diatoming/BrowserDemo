41d0fd3e56f3d21c20744197ed66e5d12abf781c
diatoming <diatoming@gmail.com>
2022-06-14 12:13:29 +0800

    Initial Commit

---
+++ BrowserDemo/BrowserDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserDemoApp.swift
+//  BrowserDemo
+//
+//  Created by Diatoming on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BrowserDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrowserDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrowserDemo
+//
+//  Created by Diatoming on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
