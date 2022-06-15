fd464bbbf69524645ef531485bfa1bb58fb34817
diatoming <diatoming@gmail.com>
2022-06-15 12:09:25 +0800

    parse firefox plugin manifest, show button on toolbar of plugin

--- BrowserDemo/ContentView.swift
+++ BrowserDemo/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var urlString = "https://addons.mozilla.org/en-US/firefox/addon/top-sites-button/"
     @State private var url: URL?
     
+    @State private var plugin: FirefoxPlugin?
+    
     var body: some View {
         VStack {
             BrowserWebView(url: $url)
@@ -19,22 +21,50 @@ struct ContentView: View {
         .frame(idealWidth: 800, maxWidth: .infinity, idealHeight: 680, maxHeight: .infinity)
         .presentedWindowToolbarStyle(.expanded)
         .toolbar {
-            Spacer()
-            TextField("", text: $urlString)
-                .frame(minWidth: 500)
-                .textFieldStyle(.roundedBorder)
-                .onSubmit {
-                    refreshUrl()
+            ToolbarItemGroup {
+                Spacer()
+                TextField("", text: $urlString)
+                    .frame(minWidth: 500)
+                    .textFieldStyle(.roundedBorder)
+                    .onSubmit {
+                        refreshUrl()
+                    }
+                
+                Spacer()
+                
+                
+                if let plugin = plugin {
+                    Button {
+                        
+                    } label: {
+                        plugin.iconImage
+                            .resizable().aspectRatio(contentMode: .fit)
+                            .help(plugin.manifest?.name ?? "Some Plugin")
+                    }
                 }
-            
-            Spacer()
+            }
         }
         .onAppear {
-            refreshUrl()            
+            refreshUrl()
+        }
+        .onReceive(NotificationCenter.default.publisher(for: .firefoxPluginInstalled)) { noti in
+            guard let plugin = noti.object as? FirefoxPlugin else {
+                return
+            }
+            self.plugin = plugin
         }
     }
     
     private func refreshUrl() {
         url = URL(string: urlString)
+    }
+}
+
+extension FirefoxPlugin: Identifiable {
+    var id: URL { url }
+    
+    var iconImage: Image {
+        guard let img = icon else { return Image(systemName: "puzzlepiece.extension.fill")}
+        return Image(nsImage: img)
     }
 }
--- BrowserDemo/FirefoxPlugin.swift
+++ BrowserDemo/FirefoxPlugin.swift
@@ -9,21 +9,55 @@ import Foundation
 import ZIPFoundation
 import AppKit
 
+struct Manifest: Codable {
+    var mainfestVersion: Int?
+    var version: String?
+    var name: String?
+    var description: String?
+    var icons: [String: String] = [:]
+}
+
 struct FirefoxPlugin {
+    /// xpi url
     let url: URL
     
-    var icon: NSImage?
+    var unzipFoldereURL: URL?
+    private(set) var icon: NSImage?
+    private(set) var manifest: Manifest?
+    
+    private var biggestIcon: NSImage? {
+        guard let url = unzipFoldereURL, let manifest = manifest else { return nil }
+        let key = manifest.icons.keys.compactMap(Int.init).sorted(by: >).first
+        guard let intKey = key,
+              let img = manifest.icons[String(intKey)] else { return nil }
+        let imgUrl = url.appendingPathComponent(img)
+        return NSImage(contentsOf: imgUrl)
+    }
     
     static func install(from url: URL) async throws {
-
-        let plugin = FirefoxPlugin(url: url)
+        
+        var plugin = FirefoxPlugin(url: url)
         
         if let plugFolder = plugin.unzip(at: url) {
-            dump(plugFolder)
+            plugin.parse(folder: plugFolder)
         }
     }
     
-    func unzip(at url: URL) -> URL? {
+    private mutating func parse(folder: URL) {
+        unzipFoldereURL = folder
+        let manifestUrl = folder.appendingPathComponent("manifest.json")
+        guard let manifestData = try? Data(contentsOf: manifestUrl) else { return }
+        let manifest = try! JSONDecoder().decode(Manifest.self, from: manifestData)
+        self.manifest = manifest
+        self.icon = icon ?? biggestIcon
+        
+        let copy = self
+        DispatchQueue.main.async {        
+            NotificationCenter.default.post(name: .firefoxPluginInstalled, object: copy)
+        }
+    }
+    
+    private func unzip(at url: URL) -> URL? {
         let fileManager = FileManager()
         let currentWorkingPath = url.deletingLastPathComponent().path
         let sourceURL = URL(fileURLWithPath: url.path)
@@ -40,4 +74,8 @@ struct FirefoxPlugin {
             return nil
         }
     }
+}
+
+extension Notification.Name {
+    static let firefoxPluginInstalled = Self("firefoxPluginInstalled")
 }
