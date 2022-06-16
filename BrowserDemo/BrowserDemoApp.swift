9f30773c2464b3d66a8a310580c8b8955057f570
diatoming <diatoming@gmail.com>
2022-06-16 08:37:24 +0800

    show tab bar by default

--- BrowserDemo/BrowserDemoApp.swift
+++ BrowserDemo/BrowserDemoApp.swift
@@ -15,18 +15,42 @@ struct BrowserDemoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(idealWidth: 800, maxWidth: .infinity, idealHeight: 800, maxHeight: .infinity)
         }
         .windowToolbarStyle(.unified)
         .windowStyle(.hiddenTitleBar)
+        .commands {
+            CommandGroup(replacing: .newItem) {}
+        }
     }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let win = NSApp.mainWindow
+        win?.tabbingMode = .preferred
+        
         TopSitesManager.shared.load()
     }
     
     func applicationWillTerminate(_ notification: Notification) {
         TopSitesManager.shared.save()
     }
+    
+}
+
+/// NSWindow accessor for SwiftUI View
+struct WindowAccessor: NSViewRepresentable {
+    @Binding var window: NSWindow?
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async {
+            self.window = view.window
+        }
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {}
 }
